a2dd94706e455c3404082a2b793933d32cbdb3e1
Byungwan Jun <unipro.kr@gmail.com>
2019-09-04 12:25:33 +0900

    Fix build error in examples on OSX

--- libsrt-sys/build.rs
+++ libsrt-sys/build.rs
@@ -3,6 +3,8 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
+
     if env::var("LIBSRT_SYS_USE_PKG_CONFIG").is_ok() {
         let mut cfg = pkg_config::Config::new();
         if let Ok(lib) = cfg.atleast_version("1.3.0").probe("srt") {
@@ -19,13 +21,38 @@ fn main() {
             .status();
     }
 
-    let mut dst = cmake::Config::new("srt")
+    let mut cmk = cmake::Config::new("srt");
+
+    if target.contains("apple") {
+        let output = Command::new("brew")
+            .args(&["--prefix", "openssl"])
+            .output()
+            .expect("failed to execute brew");
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        let trimmed_str = output_str.trim();
+
+        cmk.cflag(format!(r#"-DOPENSSL_ROOT_DIR={}"#,
+                          trimmed_str.to_string()));
+        cmk.cflag(format!(r#"-DOPENSSL_LIB_DIR={}"#,
+                          format!("{}/lib", trimmed_str)));
+        cmk.cflag(format!(r#"-DOPENSSL_INCLUDE_DIR={}"#,
+                          format!("{}/include", trimmed_str)));
+
+        println!("cargo:rustc-link-search=native={}/lib", trimmed_str);
+    }
+
+    let dst = cmk
         .define("ENABLE_SHARED", "OFF")
         .define("ENABLE_APPS", "OFF")
         .build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=srt");
-    println!("cargo:rustc-link-lib=stdc++");
+
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
     println!("cargo:rustc-link-lib=crypto");
 }
